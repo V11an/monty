2b991b58a5c0ab36dae1296bc9a4e1e23f23deaa
V11an <allankertich@gmail.com>
2023-11-19 21:42:20 +0000

    making corrections of files

---
+++ pall.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * f_pall - it prints a stack
+ * @head: head of stack
+ * @counter: no used
+ * Return: no return
+ */
+
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - it prints the top
+ * @head: head of stack
+ * @counter: line_number
+ * Return: no return
+ */
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
